76bcae8c98a77876b123e3056e71faa205976ffb
Martin Vega <mvegaca@outlook.com>
2017-09-29 11:19:38 +0200

    Add MVVMBasic Blank MasterDetail

---
+++ WTS/App/App/Views/MasterDetailDetailPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using App.Models;
+using App.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace App.Views
+{
+    public sealed partial class MasterDetailDetailPage : Page
+    {
+        public MasterDetailDetailViewModel ViewModel { get; } = new MasterDetailDetailViewModel();
+
+        public MasterDetailDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            ViewModel.Item = e.Parameter as SampleOrder;
+        }
+    }
+}
