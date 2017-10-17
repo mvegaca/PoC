6ff7936431264b10039009a5703264fdc3bb2334
Martin Vega <mvegaca@outlook.com>
2017-10-17 09:59:31 +0200

    Add ShareSource Sample

---
+++ WTS/ShareSourceSample/ShareSourceSample/ViewModels/ShareSourceViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using ShareSourceSample.Helpers;
+
+namespace ShareSourceSample.ViewModels
+{
+    public class ShareSourceViewModel : Observable
+    {
+        private ICommand _shareCommand;
+        public ICommand ShareCommand => _shareCommand ?? (_shareCommand = new RelayCommand(OnShare));
+
+        private string _selectedOption;
+        public string SelectedOption
+        {
+            get { return _selectedOption; }
+            set { Set(ref _selectedOption, value); }
+        }
+
+
+        public IEnumerable<string> ShareOptions
+        {
+            get
+            {
+                yield return "ShareSource_ShareTypeText".GetLocalized();
+                yield return "ShareSource_ShareTypeLink".GetLocalized();
+            }
+        }
+
+        public ShareSourceViewModel()
+        {
+            SelectedOption = ShareOptions.First();
+        }
+
+        private void OnShare()
+        {
+
+        }
+    }
+}
---
+++ WTS/ShareSourceSample/ShareSourceSample/Views/ShareSourcePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using ShareSourceSample.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace ShareSourceSample.Views
+{
+    public sealed partial class ShareSourcePage : Page
+    {
+        public ShareSourceViewModel ViewModel { get; } = new ShareSourceViewModel();
+
+        public ShareSourcePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
