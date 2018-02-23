a97ebd2047942c64e39788d6b1baf423451f6b83
Martin Vega <mvegaca@outlook.com>
2018-02-23 09:24:32 +0100

    Add changes

--- WTS/App1/App1/Views/BlankPage.xaml.cs
+++ WTS/App1/App1/Views/BlankPage.xaml.cs
@@ -8,6 +8,9 @@ namespace App1.Views
 {
     public sealed partial class BlankPage : Page
     {
+
+
+
         public BlankViewModel ViewModel { get; } = new BlankViewModel();
 
         public BlankPage()
