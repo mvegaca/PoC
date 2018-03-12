06d28f7ee09c6909f8b48765e10c86c51597e515
Martin Vega <mvegaca@outlook.com>
2018-03-12 15:01:33 +0100

    Add NavViewUpdate

---
+++ WTS/NavViewUpdate/NavViewUpdate/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using NavViewUpdate.Helpers;
+
+namespace NavViewUpdate.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ WTS/NavViewUpdate/NavViewUpdate/Views/ShellPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using NavViewUpdate.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace NavViewUpdate.Views
+{
+    public sealed partial class ShellPage : Page
+    {
+        public ShellViewModel ViewModel { get; } = new ShellViewModel();
+
+        public ShellPage()
+        {
+            InitializeComponent();
+            DataContext = ViewModel;
+            ViewModel.Initialize(shellFrame);
+        }
+    }
+}
