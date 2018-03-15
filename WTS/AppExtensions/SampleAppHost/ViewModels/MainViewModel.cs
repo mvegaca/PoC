873ff0f9c4ffb09da6fae4acef677372599c39ba
Martin Vega <mvegaca@outlook.com>
2018-03-15 17:11:43 +0100

    Add app extensions PoC solution

---
+++ WTS/AppExtensions/SampleAppHost/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using SampleAppHost.Helpers;
+
+namespace SampleAppHost.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
