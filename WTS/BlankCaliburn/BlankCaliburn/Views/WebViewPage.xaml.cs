cf5be4fdb24a49d6a07f18c445d1a4b48e14f7f9
Martin Vega <mvegaca@outlook.com>
2018-03-22 17:03:32 +0100

    Add out of the box App

---
+++ WTS/BlankCaliburn/BlankCaliburn/Views/WebViewPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using BlankCaliburn.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace BlankCaliburn.Views
+{
+    public sealed partial class WebViewPage : Page
+    {
+        public WebViewPage()
+        {
+            InitializeComponent();
+
+            Loaded += (s, e) => ViewModel.Initialize(webView);
+        }
+
+        private WebViewViewModel ViewModel
+        {
+            get { return DataContext as WebViewViewModel; }
+        }
+    }
+}
