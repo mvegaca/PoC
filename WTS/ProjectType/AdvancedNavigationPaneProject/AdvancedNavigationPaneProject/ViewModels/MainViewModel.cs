3d9652f99b6926f6b27fd30d0a0f66b909b44da7
Martin Vega <mvegaca@outlook.com>
2018-05-18 09:27:31 +0200

    Add advanced navigation PoC

---
+++ WTS/ProjectType/AdvancedNavigationPaneProject/AdvancedNavigationPaneProject/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Input;
+using AdvancedNavigationPaneProject.Helpers;
+using AdvancedNavigationPaneProject.Services;
+using AdvancedNavigationPaneProject.Views;
+
+namespace AdvancedNavigationPaneProject.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        private ICommand _secondShellCommand;
+
+        public ICommand SecondShellCommand => _secondShellCommand ?? (_secondShellCommand = new RelayCommand(OnSecondShell));
+
+        public MainViewModel()
+        {
+        }
+
+        private void OnSecondShell()
+        {
+            NavigationService.Navigate<SecondShellPage>(NavigationService.FrameKeyMain);
+            NavigationService.Navigate<SecondMainPage>(NavigationService.FrameKeySecondary);
+        }
+    }
+}
