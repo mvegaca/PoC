a0118be76254b3af1dbda3356fd79aba5f957c54
Martin Vega <mvegaca@outlook.com>
2018-05-24 15:21:20 +0200

    Add OldImageGalleryApp

---
+++ WTS/Pages/OldImageGalleryApp/OldImageGalleryApp/Views/ImageGalleryPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using OldImageGalleryApp.ViewModels;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace OldImageGalleryApp.Views
+{
+    public sealed partial class ImageGalleryPage : Page
+    {
+        public ImageGalleryViewModel ViewModel { get; } = new ImageGalleryViewModel();
+
+        public ImageGalleryPage()
+        {
+            InitializeComponent();
+            ViewModel.Initialize(gridView);
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (e.NavigationMode == NavigationMode.Back)
+            {
+                await ViewModel.LoadAnimationAsync();
+            }
+        }
+    }
+}
