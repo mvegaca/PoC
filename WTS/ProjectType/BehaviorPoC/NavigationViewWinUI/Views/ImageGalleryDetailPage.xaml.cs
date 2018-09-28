04e85e9578fa373faf1285a0a81d793282f99d0d
Martin Vega <mvegaca@outlook.com>
2018-09-28 09:54:59 +0200

    Add ImageGallery

---
+++ WTS/ProjectType/BehaviorPoC/NavigationViewWinUI/Views/ImageGalleryDetailPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using NavigationViewWinUI.Models;
+using NavigationViewWinUI.Services;
+using NavigationViewWinUI.ViewModels;
+
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+
+namespace NavigationViewWinUI.Views
+{
+    public sealed partial class ImageGalleryDetailPage : Page
+    {
+        public ImageGalleryDetailViewModel ViewModel { get; } = new ImageGalleryDetailViewModel();
+
+        public ImageGalleryDetailPage()
+        {
+            InitializeComponent();
+            ViewModel.SetImage(previewImage);
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await ViewModel.InitializeAsync(e.Parameter as string, e.NavigationMode);
+            showFlipView.Begin();
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            if (e.NavigationMode == NavigationMode.Back)
+            {
+                previewImage.Visibility = Visibility.Visible;
+                ViewModel.SetAnimation();
+            }
+        }
+
+        private void OnShowFlipViewCompleted(object sender, object e) => flipView.Focus(FocusState.Programmatic);
+
+        private void OnPageKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Escape && NavigationService.CanGoBack)
+            {
+                NavigationService.GoBack();
+                e.Handled = true;
+            }
+        }
+    }
+}
